2a78a98ef831888adcfaa4603ddfdc432a508049
danny_hvc <danherrerav@gmail.com>
2020-09-10 02:27:42 -0400

    :+1:

--- Code_Wars_and_More/__init__.py
+++ Code_Wars_and_More/__init__.py
@@ -6,8 +6,7 @@ from pysbox.testingallcode import *
 import pysbox.sandbox.sandbox_methods as sbm
 import pysbox.kata.katas_methods as ktm
 
-# main method
-if __name__ == "__main__":
+def main(*args: str) -> None:
     #sbm.sandycanvas()
     print(ktm.HammingCode.encode("hey"))
     # for i in range(len(ktm.HammingCode.encode("hey"))):
@@ -17,7 +16,8 @@ if __name__ == "__main__":
     parser = sbm.MyHTMLParser()
     resp = _req.get("https://partnaranimalhealth.ca/")
     print(parser.feed(resp.text))
-    
 
-"""======Unit Tests======"""
-ut.main()
+# main method
+if __name__ == "__main__":
+    main()
+    ut.main()
